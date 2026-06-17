00ca2c791709df4a3d96f767ab915561c1574fbd
actionchain <actionchain@chainguard.dev>
2026-06-17 01:07:10 +0000

    Update toshimaru/backlog-pr-link-action@v2.1.0

---
+++ __tests__/client.test.ts
@@ -0,0 +1,113 @@
+import { Client } from '../src/client'
+
+const client = new Client('xxx.backlog.com', 'dummy_key')
+
+describe('containsBacklogUrl', () => {
+  test.concurrent.each([
+    '',
+    'xxx.backlog.com',
+    'xxx.backlog.com/view/',
+    'https://xxx.backlog.com/view/',
+    'https://xxx.backlog.com/view/PROJECT',
+    'https://xxx.backlog.com/view/PROJECT',
+    'https://xxx.backlog.com/view/PROJECT1',
+    'https://xxx.backlog.com/view/PROJECT-',
+    'https://xxx.backlog.com/view/-1',
+    'https://xxx.backlog.com/view/1-X',
+    'https://xxx.backlog.com/view/X-X',
+  ])('%s does NOT contain Backlog URL', (invalidUrl) => {
+    expect(client.containsBacklogUrl(invalidUrl)).toBe(false)
+  })
+
+  test.concurrent.each([
+    'https://xxx.backlog.com/view/1-1',
+    'https://xxx.backlog.com/view/PROJECT-1',
+    ' https://xxx.backlog.com/view/PROJECT-1 ',
+    '\nhttps://xxx.backlog.com/view/PROJECT-1\n',
+  ])('%s contains Backlog URL', (validUrl) => {
+    expect(client.containsBacklogUrl(validUrl)).toBe(true)
+    expect(client.containsBacklogUrl(validUrl)).toBe(true)
+  })
+})
+
+describe('parseBacklogUrl', () => {
+  test.concurrent.each([
+    '',
+    '\n',
+    'https://xxx.backlog.com/view',
+  ])('invalid URL %#', (body) => {
+    expect(client.parseBacklogUrl(body)).toStrictEqual([])
+  })
+
+  test.concurrent.each([
+    [
+      'URL: https://xxx.backlog.com/view/PROJECT-1 ',
+      'https://xxx.backlog.com/view/PROJECT-1',
+      'PROJECT',
+      'PROJECT-1',
+    ],
+    [
+      ' hhttps://xxx.backlog.com/view/PROJECT-1x ',
+      'https://xxx.backlog.com/view/PROJECT-1',
+      'PROJECT',
+      'PROJECT-1',
+    ],
+    ['\nhttps://xxx.backlog.com/view/PJ-2\n', 'https://xxx.backlog.com/view/PJ-2', 'PJ', 'PJ-2'],
+  ])('Single URL %#', (body, url, projectId, issueId) => {
+    expect(client.parseBacklogUrl(body)).toStrictEqual([[url, projectId, issueId]])
+  })
+
+  test.concurrent.each([
+    [
+      'https://xxx.backlog.com/view/PROJECT-1 https://xxx.backlog.com/view/PJ-2',
+      'https://xxx.backlog.com/view/PROJECT-1',
+      'PROJECT',
+      'PROJECT-1',
+      'https://xxx.backlog.com/view/PJ-2',
+      'PJ',
+      'PJ-2',
+    ],
+    [
+      'https://xxx.backlog.com/view/PROJECT-1\nhttps://xxx.backlog.com/view/PJ-2',
+      'https://xxx.backlog.com/view/PROJECT-1',
+      'PROJECT',
+      'PROJECT-1',
+      'https://xxx.backlog.com/view/PJ-2',
+      'PJ',
+      'PJ-2',
+    ],
+    [
+      ' https://xxx.backlog.com/view/PROJECT-1https://xxx.backlog.com/view/PJ-2 ',
+      'https://xxx.backlog.com/view/PROJECT-1',
+      'PROJECT',
+      'PROJECT-1',
+      'https://xxx.backlog.com/view/PJ-2',
+      'PJ',
+      'PJ-2',
+    ],
+  ])('multiple URLs %#', (body, url1, projectId1, issueId1, url2, projectId2, issueId2) => {
+    expect(client.parseBacklogUrl(body)).toStrictEqual([[url1, projectId1, issueId1], [
+      url2,
+      projectId2,
+      issueId2,
+    ]])
+  })
+})
+
+describe('validateProject', () => {
+  test('invalid project', async () => {
+    expect(await client.validateProject('')).toBe(false)
+    expect(await client.validateProject('INVALID')).toBe(false)
+  })
+})
+
+describe('updateIssuePrField', () => {
+  it('failed to update', async () => {
+    const result = await client.updateIssuePrField(
+      'PROJECT',
+      'PROJECT-1',
+      'https://github.com/xxx/pull/1',
+    )
+    expect(result).toBe(false)
+  })
+})
---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import * as core from '@actions/core'
+import { context } from '@actions/github'
+import { Client } from './client'
+
+async function main() {
+  try {
+    const host = core.getInput('backlog-host', { required: true })
+    const apiKey = core.getInput('backlog-api-key', { required: true })
+
+    if (context.payload.pull_request === undefined) {
+      throw new Error('Can\'t get pull_request payload. Check your trigger is pull_request event')
+    }
+
+    const client = new Client(host, apiKey)
+    const { html_url: prUrl = '', body = '' } = context.payload.pull_request
+    if (!client.containsBacklogUrl(body)) {
+      core.info('Skip process since the body doesn\'t contain backlog URL')
+      return
+    }
+
+    for (const [backlogUrl, projectId, issueId] of client.parseBacklogUrl(body)) {
+      core.info(`Trying to link the Pull Request to ${backlogUrl}`)
+
+      if (await client.updateIssuePrField(projectId, issueId, prUrl)) {
+        core.info(`Pull Request (${prUrl}) has been successfully linked`)
+      }
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    }
+  }
+}
+
+main()
